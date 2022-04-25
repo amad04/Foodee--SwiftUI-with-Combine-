378745f044cb8fb38ba811c76120ed4afea67bae
Amad Walid <amawal@live.se>
2022-04-25 08:32:08 +0200

    Initial Commit

---
+++ Shared/FoodeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodeeApp.swift
+//  Shared
+//
+//  Created by Amad Walid on 2022-04-25.
+//
+
+import SwiftUI
+
+@main
+struct FoodeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
